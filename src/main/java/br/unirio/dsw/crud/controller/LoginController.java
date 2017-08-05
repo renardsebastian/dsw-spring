7fd7019de4fb4398396f505019a0b1853e51869c
Marcio Barros <marcio.barros@gmail.com>
2017-08-04 23:52:27 -0300

    Avancos

--- src/main/java/br/unirio/dsw/crud/controller/LoginController.java
+++ src/main/java/br/unirio/dsw/crud/controller/LoginController.java
@@ -24,6 +24,7 @@ import br.unirio.dsw.crud.utils.CryptoUtils;
 import br.unirio.dsw.crud.utils.ValidationUtils;
 import br.unirio.dsw.crud.view.login.ForgotPasswordForm;
 import br.unirio.dsw.crud.view.login.RegistrationForm;
+import br.unirio.dsw.crud.view.login.ResetPasswordForm;
 
 /**
  * Controller responsável pelas ações de login
@@ -68,7 +69,7 @@ public class LoginController
     /**
      * Ação que redireciona o usuário para a tela de criação de conta
      */
-	@RequestMapping(value = "/login/register", method = RequestMethod.GET)
+	@RequestMapping(value = "/login/create", method = RequestMethod.GET)
 	public String showRegistrationPage(WebRequest request, Model model)
 	{
         model.addAttribute("user", new RegistrationForm());
@@ -108,7 +109,7 @@ public class LoginController
  
 //        SecurityUtils.logInUser(registered);
 //        ProviderSignInUtils.handlePostSignUp(user.getEmail(), request);
-        return "redirect:/";
+        return "redirect:/login?message=login.new.account.success.created";
     }
     
 	/**
@@ -133,12 +134,15 @@ public class LoginController
 	/**
 	 * Ação que envia um token para troca de senha
 	 */
-	@RequestMapping(value = "/login/sendToken", method = RequestMethod.POST)
+	@RequestMapping(value = "/login/forgot", method = RequestMethod.POST)
 	private String sendToken(@ModelAttribute("form") RegistrationForm form, BindingResult result, WebRequest request, Locale locale)
 	{
 		if (form.getEmail().length() == 0)
 			addFieldError("email", form.getEmail(), "login.forgot.password.error.email.empty", result);
 		
+		if (!ValidationUtils.validEmail(form.getEmail()))
+			addFieldError("email", form.getEmail(), "login.forgot.password.error.email.invalid", result);
+
         if (result.hasErrors())
             return "login/forgot";
 		
@@ -155,7 +159,7 @@ public class LoginController
 			EmailService.getInstance().sendToUser(user.getName(), user.getUsername(), title, contents);
 		}
 		
-        return "redirect:/login";
+        return "redirect:/login?message=login.forgot.password.success.email.sent";
 	}
 	
 	/**
@@ -178,59 +182,51 @@ public class LoginController
 	/**
 	 * Ação que prepara o formulário de reset de senha
 	 */
-//	@RequestMapping(value = "/login/resetPassword", method = RequestMethod.GET)
-//	public String showResetPassword(@ModelAttribute("email") String email, @ModelAttribute("token") String token, BindingResult result, WebRequest request)
-//	{
-//		String token = getParameter("token", "");
-//		checkNonEmpty(token, translate("login.reset.erros.tokennaoencontrado"));
-//		
-//		String email = getParameter("email", "");
-//		checkNonEmpty(email, translate("login.reset.erros.emailnaoencontrado"));
-//
-//		Participante participante = PlataformaDAOFactory.getParticipanteDAO().getParticipanteEmail(email);
-//		check(participante != null, translate("login.reset.erros.participantenaoencontrado"));
-//
-//		boolean valido = PlataformaDAOFactory.getParticipanteDAO().verificaTokenTrocaSenha(participante.getId(), token, 72);
-//		check(valido, translate("login.reset.erros.tokeninvalido"));
-//		
-//		setAttribute("token", token);
-//		setAttribute("email", email);
-//		return SUCCESS;
-//	}
+	@RequestMapping(value = "/login/reset", method = RequestMethod.GET)
+	public String showResetPasswordPage(@ModelAttribute("email") String email, @ModelAttribute("token") String token, WebRequest request, Model model)
+	{
+		ResetPasswordForm form = new ResetPasswordForm();
+		form.setEmail(email);
+		form.setToken(token);
+        model.addAttribute("form", form);
+		return "login/reset";
+	}
 	
-//	/**
-//	 * Executa uma troca de senha baseada em reinicialização
-//	 */
-//	@DisableUserVerification
-//	@Success(type=ResultType.Redirect, value="/login/homepage.do")
-//	@Error("/jsp/plataforma/registro/resetSenha.jsp")
-//	public String executaResetSenha() throws ActionException
-//	{
-//		String token = getParameter("token", "");
-//		checkNonEmpty(token, translate("login.reset.erros.tokennaoencontrado"));
-//		
-//		String email = getParameter("email", "");
-//		checkNonEmpty(email, translate("login.reset.erros.emailnaoencontrado"));
-//
-//		setAttribute("token", token);
-//		setAttribute("email", email);
-//
-//		Participante participante = PlataformaDAOFactory.getParticipanteDAO().getParticipanteEmail(email);
-//		check(participante != null, translate("login.reset.erros.participantenaoencontrado"));
-//
+	/**
+	 * Ação que troca a senha baseada em reinicialização
+	 */
+	@RequestMapping(value = "/login/reset", method = RequestMethod.POST)
+	public String resetPassword(@ModelAttribute("form") ResetPasswordForm form, BindingResult result, WebRequest request, Locale locale)
+	{
+		if (form.getEmail().length() == 0)
+			addFieldError("newPassword", form.getEmail(), "login.reset.password.error.email.empty", result);
+		
+		if (!ValidationUtils.validEmail(form.getEmail()))
+			addFieldError("newPassword", form.getEmail(), "login.reset.password.error.email.invalid", result);
+		
+		User user = new UserDAO().getUserEmail(form.getEmail());
+
+		if (user == null)
+			addFieldError("newPassword", form.getEmail(), "login.reset.password.error.email.unrecognized", result);
+		
+		if (form.getToken().length() == 0)
+			addFieldError("newPassword", form.getToken(), "login.reset.password.error.token.empty", result);
+		
+//		TODO verifica se o token é válido
 //		boolean valido = PlataformaDAOFactory.getParticipanteDAO().verificaTokenTrocaSenha(participante.getId(), token, 72);
-//		check(valido, translate("login.reset.erros.tokeninvalido"));
-//		
-//		String novaSenha = getParameter("newpassword");
-//		check(novaSenha != null, translate("login.erros.novasenha.vazia"));
-//		check(participante.senhaAceitavel(novaSenha), translate("login.erros.novasenha.fraca"));
-//
-//		String novaSenha2 = getParameter("newpassword2");
-//		check(novaSenha2 != null, translate("login.erros.novasenha.repetevazia"));
-//		check(novaSenha.compareTo(novaSenha2) == 0, translate("login.erros.novasenha.diferente"));
-//
-//		check(PlataformaDAOFactory.getParticipanteDAO().atualizaSenha(participante.getId(), participante.getId(), novaSenha), translate("login.erros.novasenha.erro"));
-//		
-//		return addRedirectNotice("senha.trocada");
-//	}
+//		addFieldError("token", form.getToken(), "login.reset.password.error.token.invalid", result);
+		
+		if (!ValidationUtils.validPassword(form.getNewPassword()))
+			addFieldError("newPassword", form.getNewPassword(), "login.reset.password.error.password.invalid", result);
+		
+		if (!form.getNewPassword().equals(form.getRepeatNewPassword()))
+			addFieldError("repeatNewPassword", form.getNewPassword(), "login.reset.password.error.password.different", result);
+		
+        if (result.hasErrors())
+            return "login/reset";
+ 
+        String encodedPassword = passwordEncoder.encode(form.getNewPassword());
+        DAOFactory.getUserDAO().updatePassword(user.getId(), encodedPassword);
+        return "redirect:/login?message=login.reset.password.success.created";
+	}
 }
---
+++ src/main/java/br/unirio/dsw/crud/view/login/ResetPasswordForm.java
@@ -0,0 +1,16 @@
+package br.unirio.dsw.crud.view.login;
+
+import lombok.Data;
+
+/**
+ * Classe do formulário de reset de senha
+ * 
+ * @author marciobarros
+ */
+public @Data class ResetPasswordForm 
+{
+	private String email;
+	private String token;
+	private String newPassword;
+	private String repeatNewPassword;
+}
